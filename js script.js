0794a0c632e8451c27877997aee7094137084e26
mijan-design <145035680+mijan-design@users.noreply.github.com>
2023-09-14 23:48:55 +0700

    Add files via upload

---
+++ js script.js
@@ -0,0 +1,14 @@
+function luas() {
+    var s = document.getElementById('s').value ;
+
+    var luas =s*s;
+
+    document.getElementById('luas').innerHTML = luas;
+}
+function kel(){
+    var s = document.getElementById('s').value;
+
+    var kel =4*s;
+
+    document.getElementById('kel').innerHTML = kel;
+}
